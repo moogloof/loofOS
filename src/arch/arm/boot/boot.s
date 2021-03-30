cf214c74495e8e60ef698f8112c9d69c19d4619e
Ethan Yun <ethansyun@icloud.com>
2021-03-30 09:12:27 -0700

    Improved boot and link

--- src/arch/arm/boot/boot.s
+++ src/arch/arm/boot/boot.s
@@ -1,9 +1,32 @@
-.text
+.section .text.boot
 .global _start
 _start:
+	// Shutoff extra cores
+	mrc p15, 0, r5, c0, c0, 5
+	and r5, r5, #3
+	cmp r5, #0
+	bne halt
+
 	// Setup stack
-	ldr r5, =_start
-	mov sp, r5
+	ldr sp, =STACK_TOP
+
+	// BSS clear
+	ldr r4, =_sbss
+	ldr r9, =_ebss
+	mov r5, #0
+	mov r6, #0
+	mov r7, #0
+	mov r8, #0
+
+// bss clear loop
+bss_clear_loop:
+	// Store multiple
+	stmia r4!, {r5-r8}
+
+bss_loop_check:
+	// Exit once done
+	cmp r4, r9
+	blo bss_clear_loop
 
 	// Call kernel
 	bl kernel_main
