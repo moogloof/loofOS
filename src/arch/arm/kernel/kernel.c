f009c95ab158ae91ab5531bdbf2184eb6464dbd6
Ethan Yun <ethansyun@icloud.com>
2021-03-30 09:16:51 -0700

    No kernel bad boo boo ::::::::::::

--- src/arch/arm/kernel/kernel.c
+++ src/arch/arm/kernel/kernel.c
@@ -1,11 +1,6 @@
 // Include drivers
-#include <drivers/screen.h>
 
 // Main kernel func
 void kernel_main() {
-	// Reset screen
-	screen_reset();
-
 	// Hello world thing
-	screen_print("Hello, world!", 0, 0);
 }
