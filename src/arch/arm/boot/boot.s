567c7306b52e4eafd984ee1cb645f0616a14c3d0
Ethan Yun <ethansyun@icloud.com>
2021-03-30 17:08:46 -0700

    Change to boot and linker for qemu

--- src/arch/arm/boot/boot.s
+++ src/arch/arm/boot/boot.s
@@ -11,22 +11,9 @@ _start:
 	ldr sp, =STACK_TOP
 
 	// BSS clear
-	ldr r4, =_sbss
-	ldr r9, =_ebss
-	mov r5, #0
-	mov r6, #0
-	mov r7, #0
-	mov r8, #0
-
-// bss clear loop
-bss_clear_loop:
-	// Store multiple
-	stmia r4!, {r5-r8}
-
-bss_loop_check:
-	// Exit once done
-	cmp r4, r9
-	blo bss_clear_loop
+	ldr r0, =_sbss
+	ldr r1, =_ebss
+	bl memzero
 
 	// Call kernel
 	bl kernel_main
