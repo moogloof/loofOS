4e58823d398ee471d6a9236d091770d463524005
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-06-09 17:16:42 -0700

    Added IDT functionality

---
+++ src/arch/x86/core/idt.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include <core/idt.h>
+
+// The interrupt descriptor structure
+struct idt_desc {
+	uint16_t offset1;
+	uint16_t selector;
+	uint8_t unused;
+	uint8_t type:4;
+	uint8_t s:1;
+	uint8_t dpl:2;
+	uint8_t present:1;
+	uint16_t offset2;
+} __attribute__((packed)) idt[IDT_LIMIT]; // IDT buffer
+
+// Add interrupt descriptor to the IDT
+void set_id(uint8_t idt_pos, uint32_t offset, uint16_t selector, uint8_t type, uint8_t dpl, uint8_t present) {
+	// Set the ID at position in the IDT
+	idt[idt_pos] = (struct idt_desc){.offset1 = (offset + IRQ_OFFSET) & 0xffff,
+			.selector = selector,
+			.unused = 0,
+			.type = type,
+			.s = 0,
+			.dpl = dpl,
+			.present = present,
+			.offset2 = ((offset + IRQ_OFFSET) >> 16) & 0xffff};
+}
+
+// Load the interrupt descriptor table
+void load_idt() {
+	// The IDTR structure
+	struct {
+		uint16_t size;
+		uint32_t offset;
+	} __attribute__((packed)) idtr = {.size = sizeof(idt) - 1, .offset = &idt};
+
+	// Load the idtr
+	__asm__("lidt %0" : : "m"(idtr));
+}
---
+++ src/arch/x86/core/idt.h
@@ -0,0 +1,23 @@
+#ifndef _IDT_H
+#define _IDT_H
+
+#include <stdint.h>
+
+// Define IDT constraints
+#define IDT_LIMIT 256
+// Define IDT types
+#define IDT_PROT_TASK 0x5
+#define IDT_REAL_INTR 0x6
+#define IDT_REAL_TRAP 0x7
+#define IDT_PROT_INTR 0xe
+#define IDT_PROT_TRAP 0xf
+// Define IRQ vector offsets
+#define IRQ_OFFSET 0x20
+
+// Add interrupt descriptor to the IDT
+void set_id(uint8_t, uint32_t, uint16_t, uint8_t, uint8_t, uint8_t);
+
+// Load the IDT
+void load_idt();
+
+#endif
