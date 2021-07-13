27741ef6919d74681932d377c310c6a530b41bc2
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-07-12 21:23:00 -0700

    Added specific timer initializations and updater

--- src/arch/x86/drivers/timer.c
+++ src/arch/x86/drivers/timer.c
@@ -1,14 +1,34 @@
 #include <stdint.h>
+#include <core/port.h>
 #include <drivers/timer.h>
 #include <core/idt.h>
 #include <core/isr.h>
+#include <proc/queue.h>
+
+static uint8_t cur_char = 'A';
 
 // Initialize the timer interrupt
 void init_timer() {
 	// Add IRQ0
 	set_id(IRQ_OFFSET, timer_handler, 0x08, IDT_PROT_INTR, 0, 1);
+
+	// Set mode register
+	outportb(TIMER_MODE, 0b00110100);
+
+	//  Set channel0 divider for timer interrupts
+	//  Low byte
+	outportb(TIMER_CHANNEL0, 0xff);
+	// High byte
+	outportb(TIMER_CHANNEL0, 0xff);
 }
 
 // Timer handler
 __attribute__((interrupt)) void timer_handler(struct interrupt_frame* frame) {
+	display_char(cur_char++, 0, 0);
+	display_char(cur_char, 1, 0);
+	// Call switch process for stuff
+//	switch_process();
+
+	// Send EOI
+	outportb(PIC_COMMAND1, PIC_EOI);
 }
--- src/arch/x86/drivers/timer.h
+++ src/arch/x86/drivers/timer.h
@@ -3,6 +3,14 @@
 
 #include <core/isr.h>
 
+// Peripheral ports for timer
+// PIT channels
+#define TIMER_CHANNEL0 0x40
+#define TIMER_CHANNEL1 0x41
+#define TIMER_CHANNEL2 0x42
+// PIT mode
+#define TIMER_MODE 0x43
+
 // Initialize the timer interrupt
 void init_timer();
 
