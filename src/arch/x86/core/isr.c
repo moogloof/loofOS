32e464282303fc8b880286c28212f0605d22cd98
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-07-12 21:21:58 -0700

    Added port macros and other exceptions

--- src/arch/x86/core/isr.c
+++ src/arch/x86/core/isr.c
@@ -7,28 +7,28 @@
 void init_pic() {
 	// ICW1
 	// Start PIC initialization
-	outportb(0x20, 0x11);
-	outportb(0xa0, 0x11);
+	outportb(PIC_COMMAND1, 0x11);
+	outportb(PIC_COMMAND2, 0x11);
 
 	// ICW2
 	// Set the PIC vector offsets
-	outportb(0x21, IRQ_OFFSET);
-	outportb(0xa1, IRQ_OFFSET);
+	outportb(PIC_DATA1, IRQ_OFFSET);
+	outportb(PIC_DATA2, IRQ_OFFSET);
 
 	// ICW3
 	// Tell master PIC to commuicate through IRQ2 with slave PIC
-	outportb(0x21, 0b00000100);
+	outportb(PIC_DATA1, 0b00000100);
 	// Set cascade
-	outportb(0xa1, 2);
+	outportb(PIC_DATA2, 2);
 
 	// ICW4
 	// Set 8086 mode
-	outportb(0x21, 1);
-	outportb(0xa1, 1);
+	outportb(PIC_DATA1, 1);
+	outportb(PIC_DATA2, 1);
 
 	// Clear masks on all lines
-	outportb(0x21, 0);
-	outportb(0xa1, 0);
+	outportb(PIC_DATA1, 0);
+	outportb(PIC_DATA2, 0);
 }
 
 // Enable interrupts
@@ -45,10 +45,58 @@ void disable_interrupts() {
 void init_exceptions() {
 	// Set the divide-by-zero exception
 	set_id(0, divbyzero_handler, 0x08, IDT_PROT_TRAP, 0, 1);
+	set_id(1, debug_handler, 0x08, IDT_PROT_TRAP, 0, 1);
+	set_id(2, nonmask_handler, 0x08, IDT_PROT_INTR, 0, 1);
+	set_id(3, breakpoint_handler, 0x08, IDT_PROT_TRAP, 0, 1);
+	set_id(4, overflow_handler, 0x08, IDT_PROT_TRAP, 0, 1);
+	set_id(5, boundrange_handler, 0x08, IDT_PROT_TRAP, 0, 1);
+	set_id(6, invalop_handler, 0x08, IDT_PROT_TRAP, 0, 1);
+	set_id(7, nodevice_handler, 0x08, IDT_PROT_TRAP, 0, 1);
+	set_id(8, doublefault_handler, 0x08, IDT_PROT_TRAP, 0, 1);
 }
 
 // Exception handlers
 // Divide-by-zero error
 __attribute__((interrupt)) void divbyzero_handler(struct interrupt_frame* frame) {
 	kernel_panic("Division-by-zero exception occured.");
+}
+
+// Debug
+__attribute__((interrupt)) void debug_handler(struct interrupt_frame* frame) {
+	kernel_panic("Debug exception.");
+}
+
+// Non-maskable interrupt
+__attribute__((interrupt)) void nonmask_handler(struct interrupt_frame* frame) {
+	kernel_panic("Non-maskable interrupt.");
+}
+
+// Breakpoint
+__attribute__((interrupt)) void breakpoint_handler(struct interrupt_frame* frame) {
+	kernel_panic("Breakpoint exception.");
+}
+
+// Overflow
+__attribute__((interrupt)) void overflow_handler(struct interrupt_frame* frame) {
+	kernel_panic("Overflow exception.");
+}
+
+// Bound range exceeded
+__attribute__((interrupt)) void boundrange_handler(struct interrupt_frame* frame) {
+	kernel_panic("Bound range exceeded exception.");
+}
+
+// Invalid opcode
+__attribute__((interrupt)) void invalop_handler(struct interrupt_frame* frame) {
+	kernel_panic("Invalid opcode exception.");
+}
+
+// Device not available
+__attribute__((interrupt)) void nodevice_handler(struct interrupt_frame* frame) {
+	kernel_panic("Device not available exception.");
+}
+
+// Double fault
+__attribute__((interrupt)) void doublefault_handler(struct interrupt_frame* frame) {
+	kernel_panic("Double fault exception.");
 }
--- src/arch/x86/core/isr.h
+++ src/arch/x86/core/isr.h
@@ -3,14 +3,21 @@
 
 #include <stdint.h>
 
+// Define EOI code
+#define PIC_EOI 0x20
+
+// Define command and data ports
+// Master PIC command port
+#define PIC_COMMAND1 0x20
+// Master PIC data port
+#define PIC_DATA1 0x21
+// Slave PIC command port
+#define PIC_COMMAND2 0xa0
+// Slave PIC data port
+#define PIC_DATA2 0xa1
+
 // The interrupt frame struct
-struct interrupt_frame {
-	uint16_t ip;
-	uint16_t cs;
-	uint16_t flags;
-	uint16_t sp;
-	uint16_t ss;
-};
+struct interrupt_frame;
 
 // Initialize the PIC
 void init_pic();
@@ -27,5 +34,21 @@ void init_exceptions();
 // Exception handlers
 // Divide-by-zero error
 __attribute__((interrupt)) void divbyzero_handler(struct interrupt_frame*);
+// Debug
+__attribute__((interrupt)) void debug_handler(struct interrupt_frame*);
+// Non-maskable interrupt
+__attribute__((interrupt)) void nonmask_handler(struct interrupt_frame*);
+// Breakpoint
+__attribute__((interrupt)) void breakpoint_handler(struct interrupt_frame*);
+// Overflow
+__attribute__((interrupt)) void overflow_handler(struct interrupt_frame*);
+// Bound range exceeded
+__attribute__((interrupt)) void boundrange_handler(struct interrupt_frame*);
+// Invalid opcode
+__attribute__((interrupt)) void invalop_handler(struct interrupt_frame*);
+// Device not available
+__attribute__((interrupt)) void nodevice_handler(struct interrupt_frame*);
+// Double fault
+__attribute__((interrupt)) void doublefault_handler(struct interrupt_frame*);
 
 #endif
