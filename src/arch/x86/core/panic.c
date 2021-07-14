f976bca6f97cfbf00bc4c8d16704f708be9dad5d
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-07-13 21:33:35 -0700

    Panic will mask all interrupts

--- src/arch/x86/core/panic.c
+++ src/arch/x86/core/panic.c
@@ -1,9 +1,13 @@
 #include <stdint.h>
 #include <core/panic.h>
+#include <core/isr.h>
 #include <drivers/vga_text.h>
 
 // Do kernel panic function
 void kernel_panic(char* msg) {
+	// Disable interrupts
+	disable_interrupts();
+
 	// Display kernel panic message
 	reset_display();
 	set_color(0, 4);
