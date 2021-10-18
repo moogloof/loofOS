7d7b71f9f660e231455063e1af2d227ebff76ecb
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-10-18 10:54:28 -0700

    Removed unnecessary reg dump test code

--- arch/x86/drivers/timer.c
+++ arch/x86/drivers/timer.c
@@ -26,12 +26,6 @@ void init_timer() {
 
 // Timer handler
 void timer_handler(seg_register_set seg_regs, gen_register_set gen_regs, interrupt_frame frame) {
-	set_cursor_pos(2, 0);
-	kernel_print("EAX=%x, EBX=%x, ECX=%x, EDX=%x\r\n", gen_regs.eax, gen_regs.ebx, gen_regs.ecx, gen_regs.edx);
-	kernel_print("ESI=%x, EDI=%x, EBP=%x, CS=%x\r\n", gen_regs.esi, gen_regs.edi, gen_regs.ebp, frame.cs);
-	kernel_print("EIP=%x, EFLAGS=%x\r\n", frame.eip, frame.eflags);
-	kernel_print("DS=%x, ES=%x, FS=%x, GS=%x", seg_regs.ds, seg_regs.es, seg_regs.fs, seg_regs.gs);
-
 	// Send EOI
 	outportb(PIC_COMMAND1, PIC_EOI);
 }
