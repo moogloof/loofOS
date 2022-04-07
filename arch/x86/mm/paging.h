6b5cc7fbd2275fddc9e248f2886812492d511402
Ethan Yun <ethanyun@ohs.stanford.edu>
2022-04-07 09:53:38 -0700

    Added data structs for 4kib page dirs and tables

--- arch/x86/mm/paging.h
+++ arch/x86/mm/paging.h
@@ -33,6 +33,33 @@ typedef struct {
 	uint16_t lowaddr:10; // Bits 31:22 of physical address
 } __attribute__((packed)) pde_4mib;
 
+typedef struct {
+	uint8_t present:1; // Present bit; must be 1
+	uint8_t rw:1; // Read/write bit; 0 == ro, 1 == rw allowed
+	uint8_t us:1; // User/supervisor; 0 == supervisor, 1 = user
+	uint8_t pwt:1; // Page-level write-through bit
+	uint8_t pcd:1; // Page-level cache disable bit
+	uint8_t a:1; // Accessed bit
+	uint8_t ignored:1; // Ignored bit
+	uint8_t ps:1; // Page size; must be 0 for 4 KiB pages
+	uint8_t ignored2:4; // Ignored bits
+	uint32_t addr:20; // Physical address of page table
+} __attribute__((packed)) pde_4kib;
+
+typedef struct {
+	uint8_t present:1; // Present bit; must be 1
+	uint8_t rw:1; // Read/write bit; 0 == ro, 1 == rw allowed
+	uint8_t us:1; // User/supervisor; 0 == supervisor, 1 = user
+	uint8_t pwt:1; // Page-level write-through bit
+	uint8_t pcd:1; // Page-level cache disable bit
+	uint8_t a:1; // Accessed bit
+	uint8_t d:1; // Dirty bit
+	uint8_t pat:1; // PAT bit; 0
+	uint8_t g:1; // Global bit
+	uint8_t ignored:3; // Ignored bits
+	uint32_t addr:20; // Physical address of page
+} __attribute__((packed)) pte_4kib;
+
 // Initialize 32 bit paging
 void init_paging();
 
