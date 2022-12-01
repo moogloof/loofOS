b7748b295077921c29f80856441537713ff1a4ca
Ethan Yun <ethanyun@ohs.stanford.edu>
2022-11-30 19:12:38 -0800

    Added very primitive vbe driver

--- arch/x86/drivers/vbe.c
+++ arch/x86/drivers/vbe.c
@@ -1,16 +1,36 @@
 #include <stdint.h>
+#include <common.h>
 #include <drivers/vbe.h>
+#include <mm/paging.h>
 
 // The VBE info block
-struct vbe_cib __attribute__((packed)) vbe_info_block;
+static vbe_cib* vbe_info_block = 0x600 + KERNEL_BASE;
 
-// The VBE mode info block
-struct vbe_mib __attribute__((packed)) mode_info_block;
 // The saved one
-struct vbe_mib __attribute__((packed)) saved_mode_info_block;
+static vbe_mib* saved_mode_info_block = 0xa00 + KERNEL_BASE;
 
-// The dimensions of the thing
-// The selected dimensions
-uint16_t selected_width;
-uint16_t selected_height;
-uint16_t selected_vbe_mode;
+// The framebuffer in kernel memory
+volatile uint8_t* vbe_framebuffer;
+
+// The secondary framebuffer
+volatile uint8_t* vbe_framebuffer_double;
+
+// Initialize the driver
+void init_vbe() {
+	// Allocate some space to be the second buffer
+	vbe_framebuffer_double = (uint8_t*)kernel_allocate(saved_mode_info_block->bytes_per_line * saved_mode_info_block->yresolution);
+	// Allocate some kernel space to be the framebuffer
+	vbe_framebuffer = (uint8_t*)kernel_allocate(saved_mode_info_block->bytes_per_line * saved_mode_info_block->yresolution);
+	// Map space to actual physical memory
+	map_page(KERNEL_PAGE_DIRECTORY, vbe_framebuffer, saved_mode_info_block->phys_base_ptr, saved_mode_info_block->bytes_per_line * saved_mode_info_block->yresolution, 0);
+
+	// Clear the screen
+	for (int i = 0; i < saved_mode_info_block->bytes_per_line * saved_mode_info_block->yresolution; i++) {
+		vbe_framebuffer[i] = 0xff;
+	}
+}
+
+// Obtain the secondary framebuffer
+uint8_t* obtain_framebuffer() {
+	return vbe_framebuffer_double;
+}
