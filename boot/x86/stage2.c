4fd4b169e56eeef9497a6a83d014adc831ed8392
Ethan Yun <ethanyun@ohs.stanford.edu>
2023-04-17 14:50:14 -0700

    Added paging setup in boot

--- boot/x86/stage2.c
+++ boot/x86/stage2.c
@@ -3,8 +3,12 @@
 #include <vesa/vga.h>
 #include <vesa/vbe.h>
 #include <fs/exfat.h>
+#include <paging.h>
 
 extern uint16_t drive_bytes_per_sector;
+
+// Page directory table
+pde_4mib kernel_memory[1024] __attribute__((aligned(4096)));
 
 void stage2() {
 	// Size of kernel in bytes
@@ -41,8 +45,21 @@ void stage2() {
 		print("System process does not exist\r\n");
 	} else {
 		// Initial process detected
-		print("System process detected at %x. Passing info\r\n", kernel_size);
+		print("System process detected at %x. Passing info\r\n", 0x100000 + kernel_size);
 	}
+
+	// Initialize paging
+	// First do a linear map
+	for (int i = 0; i < 768; i++) {
+		kernel_memory[i] = (pde_4mib){.present = 1, .rw = 1, .us = 0, .pwt = 0, .pcd = 0, .a = 0, .d = 0, .ps = 1, .g = 0, .ignored = 0, .pat = 0, .highaddr = 0, .lowaddr = i};
+	}
+	// Map high memory
+	for (int i = 0; i < 256; i++) {
+		kernel_memory[i + 768] = (pde_4mib){.present = 1, .rw = 1, .us = 0, .pwt = 0, .pcd = 0, .a = 0, .d = 0, .ps = 1, .g = 0, .ignored = 0, .pat = 0, .highaddr = 0, .lowaddr = i};
+	}
+
+	// Enable paging
+	enable_paging(kernel_memory);
 
 	// Initialize VBE
 	init_vbe();
